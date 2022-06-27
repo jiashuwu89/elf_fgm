c2320f32c39e77b75a13c1f2d4d196d5b120580b
jwu@epss.ucla.edu <jwu@epss.ucla.edu>
2022-06-26 19:08:09 -0700

    first igrf test. get_igrf receives pos return Bxyz

--- sp/endpoints/jwu_test.py
+++ sp/endpoints/jwu_test.py
@@ -1,8 +1,8 @@
 
 from fastapi import APIRouter, Depends
 import random
-
-from ..db import science_downlink_crud
+from geopack import geopack, t89
+import datetime
 
 router = APIRouter(
     prefix="/jwu_test",
@@ -16,5 +16,21 @@ def get_two_numbers():
     b = random.randint(1,100)
     return (a,b)
 
-
-
+@router.get("/get_igrf")
+def get_igrf(xgsm: float, ygsm: float, zgsm: float):
+    """
+        Input
+        xgsm,ygsm,zgsm: The given position in cartesian GSM coordinate in Re (earth radii, 1 Re = 6371.2 km).
+        Return
+        bxgsm,bygsm,bzgsm: Cartesian GSM components of the internal magnetic field in nT.
+    """
+    t1 = datetime.datetime(2022,1,14,17,17,58)
+    t0 = datetime.datetime(1970,1,1)
+    ut = (t1-t0).total_seconds()
+    tilt = geopack.recalc(ut)
+    Re = 6371.2  # in km
+    xgsm = xgsm/Re
+    ygsm = ygsm/Re
+    zgsm = zgsm/Re
+    bxgsm,bygsm,bzgsm = geopack.igrf_gsm(xgsm,ygsm,zgsm)
+    return (bxgsm, bygsm, bzgsm)
